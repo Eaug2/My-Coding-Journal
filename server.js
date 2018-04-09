30ebde8f90d5e3de3c4e075fd42b05cbc07e67c0
Klara Boyer <kawboyer@gmail.com>
2018-04-09 15:17:00 -0400

    initial file structure and beginning of migrations folder

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("Listening on port %s " + PORT);
+  });
+});
