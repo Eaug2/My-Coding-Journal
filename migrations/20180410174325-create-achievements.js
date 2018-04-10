bd59566e9093ed32d3e6635882b1a991d4c1c430
Klara Boyer <kawboyer@gmail.com>
2018-04-10 13:44:12 -0400

    skills and achievements migrations files created

---
+++ migrations/20180410174325-create-achievements.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('achievements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      resource_name: {
+        type: Sequelize.STRING
+      },
+      resource_URL: {
+        type: Sequelize.STRING
+      },
+      comments: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('achievements');
+  }
+};
