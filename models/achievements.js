11cb8572dd7cb8e88b7eb45a4b93c3858b97f222
Klara Boyer <kawboyer@gmail.com>
2018-04-11 15:13:43 -0400

    backend controller.js functioning with postman

--- models/achievements.js
+++ models/achievements.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var achievements = sequelize.define('achievements', {
+  var achievements = sequelize.define('achievement', {
     name: DataTypes.STRING,
     type: DataTypes.STRING,
     resource_name: DataTypes.STRING,
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     status: DataTypes.STRING // This is what will be showing up with the list of goals
   }, {});
   achievements.associate = function(models) {
-    achievements.belongsToMany(models.skills, {
+    achievements.belongsToMany(models.skill, {
       through: "SkillAchievement"
     });
   };
