76c8c79d72a8247ca9f12caa366db2c329ba6c20
EllenStafford <ehs@ehstafford.com>
2018-04-12 13:35:02 -0400

    seed data completed

--- db/seeds2.sql
+++ db/seeds2.sql
@@ -1,7 +1,73 @@
 USE q877emlws16kzz65;
 
 INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
-    VALUES ('Web Accessibility 101 Bootcamp Training', 'Workshop', 'Girl DevelopIT', 'https://www.meetup.com/GDI-CLT/events/248765534/?rv=ea1&_xtd=gatlbWFpbF9jbGlja9oAJDM3OGFkMDUzLTBlM2YtNDM5MS05MDhiLTU0N2VkNzdkNDE2Yw', 'Front-end Web Development', 'Goal');
+    VALUES ('JavaScript: The Good Parts by Douglas Crockford', 'Book', 'OReilly/Yahoo Press', 'http://shop.oreilly.com/product/9780596517748.do', 'Interesting. Old from 2008.', 'Goal');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Intro to Git and GitHub', 'Workshop', 'Girl DevelopIT', 'https://www.meetup.com/GDI-CLT/events/249127606/?gj=wcs1_e&rv=wcs1_e&_xtd=gatlbWFpbF9jbGlja9oAJGJjMTkwZTE4LTg2YmItNDRmOC1hYjVlLTE5N2JjNmFmOTMwOQ&_af=event&_af_eid=249127606', 'Inexpensive review of GitHub. Looks good for networking.', 'Goal');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Deploy a Website', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'New technologies', 'Goal');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Build Front-End Web Applications from Scratch', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'Good Practice!', 'Goal');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Full Stack Flex Program', 'Course', 'Trilogy/UNCC', 'https://bootcamp.uncc.edu/coding/', 'Full Stack Web Development', 'In-progress');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Project 2: My Coding Journal', 'Project', 'Trilogy/UNCC', 'https://fast-mountain-46606.herokuapp.com/', 'Our 2nd Team Project', 'In-progress');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('A Smarter Way to Learn JavaScript by Mark Myers', 'Book', 'Mark Myers', 'http://www.asmarterwaytolearn.com/js/index-of-exercises.html', 'Great practice!', 'In-progress');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Introduction to JavaScript', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'JavaScript is difficult.', 'In-progress');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Introduction to Jquery', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'Jquery makes life easier.', 'In-progress');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Learn HTML', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'Great course!', 'Completed');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Learn CSS', 'Course', 'Code Academy', 'https://www.codecademy.com/catalog/subject/web-development', 'Good introduction', 'Completed');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Intro to HTML/CSS', 'Course', 'Khan Academy', 'https://www.khanacademy.org/', 'Excellent refresher course.', 'Completed');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('HTML/JS: Making Webpages Interactive', 'Course', 'Khan Academy', 'https://www.khanacademy.org/', 'Good course', 'Completed');
+
+INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
+    VALUES ('Project 1: RecRec App', 'Project', 'Trilogy/UNCC', 'https://bradleyaw.github.io/RecRec/', 'Our 1st Team Project', 'Completed');
 
 
-INSERT INTO skills (skills_name) VALUES ('HTML5');
+INSERT INTO skills (skills_name) VALUES ('Migrations');
+INSERT INTO skills (skills_name) VALUES ('MySQL');
+INSERT INTO skills (skills_name) VALUES ('Web-Design');
+INSERT INTO skills (skills_name) VALUES ('Team-Leader');
+INSERT INTO skills (skills_name) VALUES ('Jquery');
+INSERT INTO skills (skills_name) VALUES ('MVC');
+INSERT INTO skills (skills_name) VALUES ('HTML');
+INSERT INTO skills (skills_name) VALUES ('APIs');
+INSERT INTO skills (skills_name) VALUES ('Moment.js');
+INSERT INTO skills (skills_name) VALUES ('Bootstrap');
+INSERT INTO skills (skills_name) VALUES ('Materialize');
+INSERT INTO skills (skills_name) VALUES ('Git');
+INSERT INTO skills (skills_name) VALUES ('User-Validation');
+INSERT INTO skills (skills_name) VALUES ('Mobile-Responsive');
+INSERT INTO skills (skills_name) VALUES ('Web-Accessibility');
+INSERT INTO skills (skills_name) VALUES ('Jekyll');
+INSERT INTO skills (skills_name) VALUES ('AWS');
+INSERT INTO skills (skills_name) VALUES ('React.js');
+INSERT INTO skills (skills_name) VALUES ('Handlebars');
+INSERT INTO skills (skills_name) VALUES ('Firebase');
+INSERT INTO skills (skills_name) VALUES ('MongoDB');
+INSERT INTO skills (skills_name) VALUES ('Design-Patterns');
+INSERT INTO skills (skills_name) VALUES ('Algorithms');
+INSERT INTO skills (skills_name) VALUES ('Sequelize');
+
+
+SELECT*FROM q877emlws16kzz65.achievements;
+SELECT*FROM q877emlws16kzz65.skills;
