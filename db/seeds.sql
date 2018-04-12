7ea354f79b61bb18b3def5dff43c75f92b510384
EllenStafford <ehs@ehstafford.com>
2018-04-12 11:45:32 -0400

    update migrations to include new default and new not nulls

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE DATABASE q877emlws16kzz65;
+USE q877emlws16kzz65;
 
 INSERT INTO achievements (name, type, resource_name, resource_URL, comments, status) 
     VALUES ('Web Accessibility 101 Bootcamp Training', 'Workshop', 'Girl DevelopIT', 'https://www.meetup.com/GDI-CLT/events/248765534/?rv=ea1&_xtd=gatlbWFpbF9jbGlja9oAJDM3OGFkMDUzLTBlM2YtNDM5MS05MDhiLTU0N2VkNzdkNDE2Yw', 'Front-end Web Development', 'Goal');
@@ -10,4 +10,7 @@ INSERT INTO skills (skills_name) VALUES ('Javascript');
 INSERT INTO skills (skills_name) VALUES ('Storyboarding');
 INSERT INTO skills (skills_name) VALUES ('Team-Member');
 INSERT INTO skills (skills_name) VALUES ('Node.js');
+
+SELECT*FROM q877emlws16kzz65.achievements;
+SELECT*FROM q877emlws16kzz65.skills;
 
